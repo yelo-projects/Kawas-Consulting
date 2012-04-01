f18d88dff529f5308faa55baf58c48cd6e1c319b
Xananax Prozaxx <xananax@yelostudio.com>
2012-04-02 00:16:43 +0300

    first commit

---
+++ edit-template.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cd "$(dirname "$0")"
+
+cd themes/kawas/
+source ./edit-templates.sh
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+ini_set('display_errors', E_ALL);
+ini_set('display_startup_errors', TRUE);
+error_reporting(E_ALL);
+define('BASE_PATH', dirname(__FILE__));include ('sapphire/main.php'); 
